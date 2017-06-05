b8a6c01abe2107891dc692d97ed19c836ead154d
Luis <letorruella@gmail.com>
2017-06-05 16:09:31 -0400

    Fixed PEP8 issues

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,32 +3,26 @@ import media
 import fresh_tomatoes
 
 seven_samurai = media.Movie(
-    "Seven Samurai", 
-    "Seven Samurai help a village against bandit",
-    "http://2.bp.blogspot.com/-tbI9Z7tqdmw/T0PUbMFBROI/AAAAAAAABIY/Ip28mqmZaUQ/s1600/Shichinin-no-samurai+1.jpg", # noqa
+    "Seven Samurai", "Seven Samurai help a village against bandit",
+    "http://2.bp.blogspot.com/-tbI9Z7tqdmw/T0PUbMFBROI/AAAAAAAABIY/Ip28mqmZaUQ/s1600/Shichinin-no-samurai+1.jpg",  # noqa
     "https://www.youtube.com/watch?v=VDUjDPt9w0w&disable_polymer=true",
     "100")
 
-sanjuro = media.Movie("Sanjuro",
-    "A lonely samurai helps a family", 
-    "https://s3.amazonaws.com/criterion-production/release_boxshots/1792-c9b9b35636a0769bf266c5376a1c5692/Sanjuro_wrap-1_original.jpg", # noqa
-    "https://www.youtube.com/watch?v=ZHIRcbAMFHo","100")
+sanjuro = media.Movie("Sanjuro", "A lonely samurai helps a family", "https://s3.amazonaws.com/criterion-production/release_boxshots/1792-c9b9b35636a0769bf266c5376a1c5692/Sanjuro_wrap-1_original.jpg",  # noqa
+    "https://www.youtube.com/watch?v=ZHIRcbAMFHo", "100")
 
-casablanca = media.Movie("Casablanca",
-    "Rick Blaine who owns a nightclub in Casablanca,\
-     discovers his old flame Ilsa is in town with her husband, \
-     Victor Laszlo. Laszlo is a famed rebel, and with Germans on his tail,\
-      Ilsa knows Rick can help them get out of the country.",
-    "http://resizing.flixster.com/qwlwwAU7mmOZFuZfyJe6pJV5CAE=/800x1200/v1.bTsxMTE2ODEwMTtqOzE3NDEwOzIwNDg7ODAwOzEyMDA", #noqa
-    "https://www.youtube.com/watch?v=PD9HDwMcfRE&disable_polymer=true","97"
-
-)
+casablanca = media.Movie("Casablanca", "Rick Blaine who owns a nightclub in \
+    Casablanca, discovers his old flame Ilsa is in town with her husband, \
+    Victor Laszlo. Laszlo is a famed rebel, and with Germans on his tail,\
+    Ilsa knows Rick can help them get out of the country.",
+    "http://resizing.flixster.com/qwlwwAU7mmOZFuZfyJe6pJV5CAE=/800x1200/v1.bTsxMTE2ODEwMTtqOzE3NDEwOzIwNDg7ODAwOzEyMDA",  # noqa
+    "https://www.youtube.com/watch?v=PD9HDwMcfRE&disable_polymer=true", "97")
 
 toy_story = media.Movie(
     "Toy Story",
     "A story about a kid and his toys that come to life",
-    "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450",#noqa
-    "https://www.youtube.com/watch?v=vwyZH85NQC4&disable_polymer=true","100"
+    "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450",  # noqa
+    "https://www.youtube.com/watch?v=vwyZH85NQC4&disable_polymer=true", "100"
 )
 
 akira = media.Movie(
@@ -36,28 +30,28 @@ akira = media.Movie(
     "Shotaro Kaneda, a leader of a local biker gang who must stop\
      his friend Tetsuo Shima from using his newly awakened superpowers \
      to release the titular esper",
-    "http://t3.gstatic.com/images?q=tbn:ANd9GcS6hoa1ePkqSULxJOKdO3O1PriRxrl5bOYT5owZJsh7FDwCPDrW" , #noqa,
-    "https://www.youtube.com/watch?v=7mdMtuGL7eg","87")
+    "http://t3.gstatic.com/images?q=tbn:ANd9GcS6hoa1ePkqSULxJOKdO3O1PriRxrl5bOYT5owZJsh7FDwCPDrW",  # noqa,
+    "https://www.youtube.com/watch?v=7mdMtuGL7eg", "87")
 
 paprika = media.Movie(
     "Paprika",
     'Dr. Atsuko Chiba works as a scientist by day and, under the code name\
      "Paprika" is a dream detective at night',
     "http://img.moviepostershop.com/paprika-movie-poster-2006-1020689313.jpg",
-    "https://www.youtube.com/watch?v=1wOrvd_ylDY","84")
+    "https://www.youtube.com/watch?v=1wOrvd_ylDY", "84")
 
 high_low = media.Movie(
     "High and Low",
     "Toshiro Mifune stars as a wealthy industrialist whose family becomes\
      the target of a ruthless kidnapper",
     "http://www.impawards.com/1963/posters/high_and_low.jpg",
-    "https://www.youtube.com/watch?v=LV3z2Ytxu90","94")
+    "https://www.youtube.com/watch?v=LV3z2Ytxu90", "94")
 
 up = media.Movie(
     "UP",
     "A 78-year-old balloon salesman, is about to fulfill a lifelong dream",
-    "http://static.rogerebert.com/uploads/movie/movie_poster/up-2009/large_zh9DXJhBdHVVaWiDURTipADamcK.jpg", #noqa
-    "https://www.youtube.com/watch?v=BDCSncUZxvs&disable_polymer=true","98")
+    "http://static.rogerebert.com/uploads/movie/movie_poster/up-2009/large_zh9DXJhBdHVVaWiDURTipADamcK.jpg",  # noqa
+    "https://www.youtube.com/watch?v=BDCSncUZxvs&disable_polymer=true", "98")
 
 list_movies = [
     seven_samurai,
--- media.py
+++ media.py
@@ -1,26 +1,20 @@
 import webbrowser
 
-#create a class movie to organize the properties of an object
+# create a class movie to organize the properties of an object
 
 
-#class
+# class
 class Movie():
-    """ function to "init"iate  the class """  
-    def __init__(self, title, 
-                movie_story, poster_image_url , 
-                trailer_youtube_url, movie_rating):
+    """ function to "init"iate  the class """
+    def __init__(self, title, movie_story, poster_image_url,
+                 trailer_youtube_url, movie_rating):
 
             self.title = title
             self.movie_story = movie_story
-            self.poster_image_url = poster_image_url 
+            self.poster_image_url = poster_image_url
             self.trailer_youtube_url = trailer_youtube_url
             self.movie_rating = movie_rating
 
-#fuction to call YouTube trailer 
+# fuction to call YouTube trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
-
-
-
-          
